8a7183ca0c5bfc9f7c539bb4390837ab962f0c3e
Mateus de Souza Almeida <69724001+mattsouza0@users.noreply.github.com>
2021-05-25 20:48:35 -0300

    Add files via upload

---
+++ ZenitPolar.asm
@@ -0,0 +1,58 @@
+section .data 
+chv1 db " ZENITzenit-POLARpolar" 
+lim equ $-chv1 
+chv2 db "-POLARpolar ZENITzenit" 
+
+section .bss 
+strec resb 100 
+tam resd 1
+
+section .text
+global _start 
+_start: 
+
+;receber a string
+mov eax,3 
+mov ebx,0 
+mov ecx,strec 
+mov edx,100
+int 0x80 
+
+mov [tam],eax 
+;while
+xor esi,esi 
+while:
+mov al,[strec+esi]
+cmp al,10 
+je fim 
+
+;for
+xor edi,edi 
+for:
+cmp al,[chv1+edi] 
+je sim 
+
+nao: 
+inc edi 
+cmp edi,lim 
+je jump 
+jmp for 
+
+sim:
+mov al,[chv2,edi] 
+mov [strec,esi],al 
+
+jump:
+inc esi 
+jmp while
+
+fim:
+; print da string criptografata
+mov eax,4 
+mov ebx,1 
+mov ecx,strec 
+mov edx,[tam] 
+int 0x80 
+mov eax,1 
+; syscall
+int 0x80 
